feb0a94e04d554d10f87a66ca0947ff76a4d3c2c
Vladislav <v39575499@gmail.com>
2025-09-21 02:08:41 +0200

    ver 73

--- bootstrap_wan_talk_ver15.sh
+++ bootstrap_wan_talk_ver15.sh
@@ -19,11 +19,11 @@ log "Supervisor is running."
 log "Stopping services..."
 supervisorctl stop comfyui caddy sshd syncthing || true
 
-log "Running provision script for wan_talk_ver4.0"
-if curl -fsSL --retry 5 https://raw.githubusercontent.com/bidzy-app/config_vast/main/wan_talk_ver4.0.sh -o /tmp/provision.sh; then
+log "Running provision script for wan_talk_ver4.1"
+if curl -fsSL --retry 5 https://raw.githubusercontent.com/bidzy-app/config_vast/main/wan_talk_ver4.1.sh -o /tmp/provision.sh; then
   bash /tmp/provision.sh >>/var/log/onstart_provision.log 2>&1
 else
-  log "ERROR: Failed to download wan_talk_ver4.0.sh"
+  log "ERROR: Failed to download wan_talk_ver4.1.sh"
 fi
 
 log "Starting comfyui via supervisor"
